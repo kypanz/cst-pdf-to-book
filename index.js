b870a18e62559afc8122ba3dddcfcfd8f40024b6
kypanz <kypanz@pop-os.localdomain>
2024-05-11 12:15:15 -0300

    Fixing the content of the book

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { PdfReader } from "pdfreader";
+import fs from "fs";
 const FOLDER_PATH = "./files";
+const NAME_BOOK = "example";
 
 async function canYouRead() {
   try {
@@ -10,7 +12,7 @@ async function canYouRead() {
     let current_text = "";
     await new Promise((resolve, reject) => {
       new PdfReader({}).parseFileItems(
-        `${FOLDER_PATH}/example.pdf`,
+        `${FOLDER_PATH}/${NAME_BOOK}.pdf`,
         (err, item) => {
           if (err) {
             console.error("error:", err);
@@ -19,7 +21,7 @@ async function canYouRead() {
           if (!item) {
             // Adding the last page and then resolve
             book.pages[current_page] = current_text;
-            console.warn("end of file");
+            // console.warn("end of file");
             resolve();
           } else if (
             item &&
@@ -28,15 +30,25 @@ async function canYouRead() {
           ) {
             book.pages[current_page] = current_text;
             current_page = item.page;
+            current_text = '';
           } else if (item.text && item.text != undefined) {
             current_text += item.text;
           }
         },
       );
     });
-    console.log(book.pages[1]);
+    console.log(book.pages[5]);
+    // createExtractedBook({ name_book: NAME_BOOK, book: book });
   } catch (error) {
-    console.error("can't read the pdf");
+    console.error("can't read the pdf", error);
+  }
+}
+
+function createExtractedBook({ name_book, book }) {
+  try {
+    fs.writeFileSync(`./books/${name_book}.json`, JSON.stringify(book));
+  } catch (error) {
+    console.error("[ createExtractedBook | error ] => ", error);
   }
 }
 
